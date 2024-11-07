8e5c462e4e4012ba0162ff83891af6029d0eeb43
ElodieLS <elodie.migeon@learningsalad.fr>
2024-11-07 18:07:06 +0100

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,69 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6XdFdZeIkpI":
+        Script1();
+        break;
+      case "5YDWVXw8Wcv":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('656aW8hlErO');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6nj2nAbO2CD';
+const shakeAmount = 2;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+window.Script2 = function()
+{
+  const target = object('656aW8hlErO');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6nj2nAbO2CD';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
